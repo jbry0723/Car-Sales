d65e69dc4a23ad73def903d661b341db5f9abe42
Justin Justin <jbry0723@gmail.com>
2020-12-09 00:52:08 -0500

    close

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,31 @@
 import React from 'react';
-
+import {subtractFeature} from "../actions/carAction"
+import {connect} from "react-redux"
 const AddedFeature = props => {
+ 
+
+  const handleClick = e => {
+    e.preventDefault();
+    props.subtractFeature(props.feature, props.car.features)
+}
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button onClick={handleClick} className="button">X</button>
+      
       {props.feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+
+
+const mapStateToProps=state=> {
+  return{
+  car:state.car,
+  additionalFeatures:state.additionalFeatures,
+  additionalPrice:state.additionalPrice
+  }
+}
+
+export default connect(mapStateToProps,{subtractFeature} )(AddedFeature)
